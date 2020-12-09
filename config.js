41aa81a1cb6c5157f0fbe0d5fb6301affe3215ea
Omar BOUSSARSAR <boussarsar.omar@gmail.com>
2020-12-09 00:46:39 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteTitle: 'Welcome!', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Omar',
+  lastName: 'BOUSSARSAR',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/omarboussarsar',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/omarboussarsar/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/omarboussarsar',
+    },
+    {
+      icon: 'fa-symfony',
+      name: 'Profile Symfony',
+      url: 'https://connect.symfony.com/profile/omarboussarsar',
+    },
+  ],
+  email: 'contact@omarboussarsar.com',
+  //phone: '000-00000',
+  address: 'Paris, France',
+  job: 'Web Development Engineer PHP/Symfony',
+  about: 'In four words: I am passionately curious.',
+};
